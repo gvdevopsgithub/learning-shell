eec8d7dc14737f396916261e76893beaff636ce6
gv <gvdevops00@gmail.com>
2022-07-03 22:07:55 +0530

    varable

---
+++ 03.varaibles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Declare variable
+a=100
+
+# print data in variable
+echo $a
+
+# Arithmetic substitution
+b=$((3=5))
+
+echo $b
+
+#command substitution
+DATE=$(date +%F)
+
+echo Welcome, Today date is $DATE
