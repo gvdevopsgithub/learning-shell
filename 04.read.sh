98a5fe3bc8a9a3a6ae90b9e07933f77604e4e8cb
gv <gvdevops00@gmail.com>
2022-07-04 15:35:22 +0530

    read demo

---
+++ 04.read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p 'Enter your name: ' name
+echo -e "Hey $name, Welcome to DevOps Training..."
+
+read -p 'enter your phone number: ' phone
+echo "thanks for entering details,will text you details soon to $phone"
