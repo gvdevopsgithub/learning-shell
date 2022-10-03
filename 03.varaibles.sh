42c0ed81af24e9e80e688b59e6c436ab9b413d15
gv <gvdevops00@gmail.com>
2022-10-03 19:45:11 +0530

    push

--- 03.varaibles.sh
+++ 03.varaibles.sh
@@ -1,22 +1,27 @@
-#!/bin/bash
+##!/bin/bash
+#
+## Declare variable
+#a=100
+#
+## print data in variable
+#echo $a
+#
+## Arithmetic substitution
+#b=$((3+5))
+#
+#echo $b
+#
+#
+##command substitution
+#DATE=$(date +%F)
+#
+#echo Welcome, Today date is $DATE
+#
+#
+## variable from command line
+#echo c = $c
 
-# Declare variable
 a=100
-
-# print data in variable
 echo $a
-
-# Arithmetic substitution
-b=$((3+5))
-
+b=$((5+8))
 echo $b
-
-
-#command substitution
-DATE=$(date +%F)
-
-echo Welcome, Today date is $DATE
-
-
-# variable from command line
-echo c = $c
