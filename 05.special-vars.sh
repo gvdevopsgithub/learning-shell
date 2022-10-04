3a871297d69151c65efd605ce04ae1148488fd76
gv <gvdevops00@gmail.com>
2022-10-04 09:51:14 +0530

    push

--- 05.special-vars.sh
+++ 05.special-vars.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+#
+#echo Script Input = $0
+#echo First Input = $1
+#echo Second Input = $2
+#echo All Inputs = $*
+#echo Number of Inputs = $#
+
+
 
 echo Script Input = $0
 echo First Input = $1
 echo Second Input = $2
 echo All Inputs = $*
-echo Number of Inputs = $#
 echo Number of Inputs = $#
