9c0b915313f6194871383a7c72dfa9ccdad0c140
gv <gvdevops00@gmail.com>
2022-10-05 10:11:13 +0530

    push

--- 05.special-vars.sh
+++ 05.special-vars.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-#
-#echo Script Input = $0
-#echo First Input = $1
-#echo Second Input = $2
-#echo All Inputs = $*
-#echo Number of Inputs = $#
-
-
 
 echo Script Input = $0
 echo First Input = $1
 echo Second Input = $2
 echo All Inputs = $*
 echo Number of Inputs = $#
+
+
+# From 10th value we need to access the variables as ${10}} instead of $10, Because $10 is nothing but $1+0
